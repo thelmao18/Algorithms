39050c42cb55fd3125962e033c33198abbcb4e4b
Thelma Olafsdottir <thelmaolafsdottir@Thelmas-MacBook-Air.local>
2018-09-04 14:42:10 +0000

    Input

--- max_int.py
+++ max_int.py
@@ -1,1 +1,8 @@
-#comment
+num_int = int(input("Input a number: "))    # Do not change this line
+
+# Fill in the missing code
+max_int = 0
+
+while num_int 
+
+print("The maximum is", max_int)    # Do not change this line
