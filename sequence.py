23c0088beaef48dc3cc9c3de65f3ba0c7e3a2a6d
Thelma Olafsdottir <thelmaolafsdottir@Thelmas-MacBook-Air.local>
2018-09-04 18:15:20 +0000

    Working program

--- sequence.py
+++ sequence.py
@@ -2,3 +2,27 @@
 #Step 2: Make the sequence 1, 2, 3, 6, 11, 20, 37, ___, ___, ___, …
 #Step 3: The program prints the first n numbers in the sequence: 1, 2, 3, 6, 11, 20, 37, ___, ___, ___, …
 
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+x_int = 0
+a_int = 0
+b_int = 0
+c_int = 0
+
+for i in range(1, n+1):
+    if i == 1:
+        a_int = i
+        print(a_int)
+    elif i == 2:
+        b_int = i
+        print(b_int)
+    elif i == 3:
+        c_int = i
+        print(c_int)
+    else:
+        x_int = a_int + b_int + c_int
+        print(x_int)
+        
+        a_int = b_int
+        b_int = c_int
+        c_int = x_int
