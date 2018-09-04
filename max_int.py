cbdfc3772947c713bf31772cfeae479965d0eb93
Thelma Olafsdottir <thelmaolafsdottir@Thelmas-MacBook-Air.local>
2018-09-04 14:55:27 +0000

    Assign integers to variables

--- max_int.py
+++ max_int.py
@@ -2,7 +2,6 @@ num_int = int(input("Input a number: "))    # Do not change this line
 
 # Fill in the missing code
 max_int = 0
-
-while num_int 
+int_stop_num = -1
 
 print("The maximum is", max_int)    # Do not change this line
