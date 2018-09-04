5e8f089a69a2703114edf2463fdee145b1c9f625
Thelma Olafsdottir <thelmaolafsdottir@Thelmas-MacBook-Air.local>
2018-09-04 16:27:09 +0000

    Workingprogram

--- max_int.py
+++ max_int.py
@@ -1,3 +1,15 @@
 #Step 1: The user repeatedly inputs numbers until a negative value is entered.
 #Step 2: When negative value is entered find the maximum positive integer input by the user.
 #Step 3: Print the maximum number.
+
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+max_int = 0
+
+while num_int >= 0:
+    if max_int < num_int:
+        max_int = num_int
+    num_int = int(input("Input a number: "))
+    
+
+print("The maximum is", max_int)    # Do not change this line
